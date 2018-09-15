9cf5bd923b68e96ff9d6a8cc6d22ca884c768159
vitalij.kiktenko <vitalij.kiktenko@murka.com>
2018-09-16 00:29:16 +0300

    Add Asset Bundle Browser Add logic for Loading Bundles from server

---
+++ E_Stories/Assets/Scripts/LoadBundle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadBundle : MonoBehaviour
+{
+	public string URL;
+	public string ObjectName;
+	
+	private IEnumerator Start () 
+	{
+		WWW www = new WWW(URL);
+		while (!www.isDone)
+		{
+			yield return null;
+		}
+		AssetBundle myasset = www.assetBundle;
+		GameObject object3d = myasset.LoadAsset<GameObject>(ObjectName);
+		Instantiate(object3d);
+	}
+}
