22e9dfa1ae0951d6a8aa85293347c53cf6bf71eb
vk <kiktenkovitaly@gmail.com>
2018-09-24 19:32:51 +0300

    Data base working

---
+++ FR/Assets/Scripts/DataLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+	public string[] Items;
+
+	private IEnumerator Start () 
+	{
+		WWW itemsData = new WWW("http://127.0.0.1:8080/fr_database/ItemsData.php");
+		yield return itemsData;
+		string itemsDataString = itemsData.text;
+		Debug.Log(itemsDataString);
+
+		Items = itemsDataString.Split(';');
+		Debug.Log(GetDataValue(Items[0], "Text:"));
+	}
+
+	private string GetDataValue(string data, string index)
+	{
+		string value = data.Substring(data.IndexOf(index) + index.Length);
+		if (value.Contains("|"))
+		{
+			value = value.Remove(value.IndexOf("|"));
+		}
+		return value;
+	}
+}
